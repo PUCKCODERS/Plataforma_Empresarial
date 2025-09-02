895db668ff81193dbe74fc15100a977a5663a9ee
PUCKCODERS <export async function verifyForgotPasswordOtp(request, response) {>
2025-09-02 05:18:59 -0500

    Terminado hasta projectos

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+// Nanbar
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+// End of Nanbar
+
+// Projects
+const filterLinks = document.querySelectorAll(".filter-nav-link");
+
+filterLinks.forEach((filterLink) => {
+  filterLink.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    document
+      .querySelector(".filter-nav-link.active")
+      .classList.remove("active");
+    filterLink.classList.add("active");
+
+    const projects = document.querySelectorAll(".project");
+    projects.forEach((project) => {
+      project.classList.add("hide");
+
+      if (
+        filterLink.getAttribute("data-type") ===
+          project.getAttribute("data-type") ||
+        filterLink.getAttribute("data-type") === "all"
+      ) {
+        project.classList.remove("hide");
+      }
+    });
+  });
+});
+
+// End of Projects
